10b2bac2f136e81c78b090d51163dceb7248779b
ErikLarsson19 <102734798+ErikLarsson19@users.noreply.github.com>
2024-05-20 13:41:46 +0200

    inital setup of infura calls

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+  const [contractData, setContractData] = useState('');
+
+  useEffect(() => {
+    const loadBlockchainData = async () => {
+      const network = process.env.REACT_APP_ETHEREUM_NETWORK;
+      const infuraApiKey = process.env.REACT_APP_INFURA_API_KEY;
+    
+      if (!network || !infuraApiKey) {
+        console.error('Missing environment variables for network or Infura API key');
+        return;
+      }
+
+      const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+          `https://${network}.infura.io/v3/${infuraApiKey}`
+        )
+      );
+
+      // Define the contract ABI and address
+      const contractAddress = '0xE7307dd81273F2a40313636485FE5183268CF094';
+      const contractABI = [
+        {
+          "constant": true,
+          "inputs": [
+            {
+              "name": "_id",
+              "type": "uint256"
+            }
+          ],
+          "name": "getBookContent",
+          "outputs": [
+            {
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ];
+
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+      // Call the contract function and set the state with the result
+      try {
+        const bookId = 0; //Hardcoded for now
+        const data = await contract.methods.getBookContent(bookId).call();
+        setContractData(data);
+      } catch (error) {
+        console.error('Error fetching contract data:', error);
+      }
+    };
+
+    loadBlockchainData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ShortBooks</h1>
+        {contractData && <p>Book Content: {contractData}</p>}
       </header>
     </div>
   );
